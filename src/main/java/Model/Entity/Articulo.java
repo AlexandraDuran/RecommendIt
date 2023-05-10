0f1cd595ff94991605631649d60b1b66b6bbfccc
Anyela Herrera <Anyela Herrera@LAPTOP-15HJCT3V>
2023-05-09 19:51:00 -0500

    Se sube primer borrador de proyecto RecommendIt con el paquete Moel.Entity, en el que se encuentra la clase articulo y usuario. Importante: se tienen algunas dudas sobre las llaves foraneas y si faltan clases.

---
+++ src/main/java/Model/Entity/Articulo.java
@@ -0,0 +1,93 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model.Entity;
+
+/**
+ *
+ * @author Anyela Herrera
+ */
+public class Articulo {
+
+    private String titulo;
+    private String foto;
+    private String descripcion;
+    private String id;
+    private String id_comentario;
+    private String id_usuario;
+
+    public Articulo() {
+    }
+
+    public Articulo(String titulo, String foto, String descripcion, String id, String id_comentario, String id_usuario) {
+        this.titulo = titulo;
+        this.foto = foto;
+        this.descripcion = descripcion;
+        this.id = id;
+        this.id_comentario = id_comentario;
+        this.id_usuario = id_usuario;
+    }
+
+    public Articulo(String id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId_comentario() {
+        return id_comentario;
+    }
+
+    public void setId_comentario(String id_comentario) {
+        this.id_comentario = id_comentario;
+    }
+
+    public String getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(String id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    @Override
+    public String toString() {
+        return "Articulo{" + "titulo=" + titulo + 
+                ", foto=" + foto + ", descripcion=" 
+                + descripcion + ", id=" + id +
+                ", id_comentario=" + id_comentario + 
+                ", id_usuario=" + id_usuario + '}';
+    }
+
+}
---
+++ src/main/java/Model/Entity/Usuario.java
@@ -0,0 +1,82 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model.Entity;
+
+/**
+ *
+ * @author Anyela Herrera
+ */
+public class Usuario {
+    private String nombre;
+    private String correo;
+    private String usuario;
+    private String contraseña;
+    private String id;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nombre, String correo, String usuario, String contraseña, String id) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+        this.id = id;
+    }
+    
+    public Usuario (String id){
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "nombre=" + nombre + 
+                ", correo=" + correo + 
+                ", usuario=" + usuario + 
+                ", contrase\u00f1a=" + contraseña + 
+                ", id=" + id + '}';
+    }
+    
+}
